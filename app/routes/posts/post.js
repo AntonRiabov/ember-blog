5058ffef1c47abf2a425b95482eb7554b4364e19
Anton Riabov <anton.riabov@engagepoint.com>
2016-06-01 18:58:41 +0300

    add posts route dir

---
+++ app/routes/posts/post.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  //model: function(){
+    //console.log( 'id of post : ' + params.post_id);
+    //console.log('record by id : ' + this.store.findRecord('post', params.post_id));
+    //return this.store.findRecord('post', params.post_id);
+  //}
+});
